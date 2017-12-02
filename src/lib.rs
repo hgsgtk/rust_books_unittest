17a9263a3cfcfcbfd0e184f5b56900e08a3aa8f8
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-02 14:07:59 +0900

    chapter/11.1 implement panic test

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,25 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
+
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -51,5 +70,11 @@ mod tests {
     fn greeting_contains_name() {
         let result = greeting("Carol");
         assert!(result.contains("Carol"), "Greeting did not contain name, value was `{}`", result);
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
